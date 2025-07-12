30d141f0bf623213fc811039b2b2dd6e323b6283
Yuval Nahshony <200560745@student.georgianc.on.ca>
2025-07-12 16:19:07 -0400

    basic functionality done

--- script.js
+++ script.js
@@ -1,6 +1,31 @@
-document.getElementById("smoothieForm").addEventListener("submit", function (e) {
-    e.preventDefault();
-    const formData = new FormData(this);
-    alert('name: ', formData.get("name"));
+class Smoothie{
+    constructor(name, size, main, fruit){
+        this.name = name;
+        this.size = size;
+        this.main = main;
+        this.fruit = fruit;
+    }
 
+    
+
+}
+class smoothieOrder extends Smoothie {
+    constructor(name, size, main, fruit){
+        super(name, size, main, fruit);
+    }
+    description(){
+        return `name: ${this.name}, ordered a ${this.size} ${this.fruit} smoothie with ${this.main}`;
+    };
+}
+
+document.getElementById("smoothieForm").addEventListener('submit', e => {
+    e.preventDefault();
+
+    const name = document.getElementById("name").value;
+    const size = document.getElementById("size").value;
+    const main = document.getElementById("main").value;
+    const fruit = document.getElementById("fruit").value;
+
+    const order = new smoothieOrder(name, size, main, fruit);
+    document.getElementById("result").textContent = order.description();
 });
