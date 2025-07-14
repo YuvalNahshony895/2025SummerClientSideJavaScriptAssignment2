f62e384e11f4fa0e1c9454d8290495af45df8f63
Yuval Nahshony <200560745@student.georgianc.on.ca>
2025-07-14 13:39:08 -0400

    added pricing and bill

--- script.js
+++ script.js
@@ -10,22 +10,71 @@ class Smoothie{
 
 }
 class smoothieOrder extends Smoothie {
-    constructor(name, size, main, fruit){
+    constructor(name, size, main, fruit, sizePrice, mainPrice, fruitPrice, Total){
         super(name, size, main, fruit);
+        this.sizePrice = sizePrice.toFixed(2);
+        this.mainPrice = mainPrice.toFixed(2);
+        this.fruitPrice = fruitPrice.toFixed(2);
+        this.Total = Total;
+        this.formattedTotal = this.Total.toFixed(2);
     }
     description(){
-        return `${this.name} ordered a ${this.size} ${this.fruit} smoothie with ${this.main}`;
+        return `${this.name} ordered a ${this.size} ${this.fruit} smoothie with ${this.main}, for a total of ${this.formattedTotal}`;
+    };
+    bill(){
+        return `size: ${this.size} (${this.sizePrice})\nSmoothie Base: ${this.main} (${this.mainPrice})\nFruit: ${this.fruit} (${this.fruitPrice})`;
     };
 }
 
 document.getElementById("smoothieForm").addEventListener('submit', e => {
     e.preventDefault();
 
-    const name = document.getElementById("name").value;
-    const size = document.getElementById("size").value;
-    const main = document.getElementById("main").value;
-    const fruit = document.getElementById("fruit").value;
+    let name = document.getElementById("name").value;
+    let size = document.getElementById("size").value;
+    let main = document.getElementById("main").value;
+    let fruit = document.getElementById("fruit").value;
+    let sizePrice = 0;
+    let mainPrice = 0;
+    let fruitPrice = 0;
 
-    const order = new smoothieOrder(name, size, main, fruit);
+    
+    
+    if (size == "Small"){
+        sizePrice = 2;
+    }
+    else if (size == "Medium"){
+        sizePrice = 2.5;
+    }
+    else if (size == "Large"){
+        sizePrice = 3;
+    };
+    if (main == "Almond Milk"){
+        mainPrice = 1.25;
+    }
+    else if (main == "Yogurt"){
+        mainPrice = 1;
+    }
+    else if (main == "Coconut Water"){
+        mainPrice = 1.5;
+    }
+    else if (main == "Orange Juice"){
+        mainPrice = 1;
+    };
+    if (fruit == "Banana"){
+        fruitPrice = 0.75;
+    }
+    else if (fruit == "Strawberry"){
+        fruitPrice = 1.25;
+    }
+    else if (fruit == "Blueberry"){
+        fruitPrice = 1.5;
+    }
+    else if (fruit == "Mango"){
+        fruitPrice = 1.25;
+    };
+    let Total = sizePrice + mainPrice + fruitPrice;
+
+    const order = new smoothieOrder(name, size, main, fruit, sizePrice, mainPrice, fruitPrice, Total);
     document.getElementById("result").textContent = order.description();
+    document.getElementById("bill").textContent = order.bill();
 });
