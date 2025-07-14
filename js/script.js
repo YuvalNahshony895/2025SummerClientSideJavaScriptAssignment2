e6c32c32f800c22ee8ab2ee3300e0e0585f185d0
Yuval Nahshony <200560745@student.georgianc.on.ca>
2025-07-14 14:22:26 -0400

    added images that show acoording to what the customer orders.

--- js/script.js
+++ js/script.js
@@ -19,11 +19,42 @@ class smoothieOrder extends Smoothie {
         this.formattedTotal = this.Total.toFixed(2);
     }
     description(){
-        return `${this.name} ordered a ${this.size} ${this.fruit} smoothie with ${this.main}, for a total of ${this.formattedTotal}`;
+        return `${this.name} ordered a ${this.size} ${this.fruit} smoothie with ${this.main}, for a total of $${this.formattedTotal}`;
     };
     bill(){
         return `size: ${this.size} (${this.sizePrice})\nSmoothie Base: ${this.main} (${this.mainPrice})\nFruit: ${this.fruit} (${this.fruitPrice})`;
     };
+    serveIt(){
+
+    let smoothieCup = document.createElement("img");
+    if (this.size == "Small"){
+        smoothieCup.setAttribute("height", "100");
+    }
+    else if (this.size == "Medium"){
+        smoothieCup.setAttribute("height", "150");
+    }
+    else if (this.size == "Large"){
+        smoothieCup.setAttribute("height", "200");
+    };
+    if (this.fruit == "Banana"){
+        smoothieCup.setAttribute("src", "/images/banana.png");
+        document.getElementById("imageAttr").innerHTML="<a href='https://pngtree.com/freepng/banana-smoothie-drink-composition-watercolor_14631298.html'>png image from pngtree.com/</a>"
+    }
+    else if (this.fruit == "Strawberry"){
+        smoothieCup.setAttribute("src", "/images/strawberry.png");
+        document.getElementById("imageAttr").innerHTML="<a href='https://https://www.freepik.com/free-vector/hand-painted-watercolor-smoothies-blender-glass-illustration_13839324.htm#fromView=keyword&page=1&position=0&uuid=dae3cd3d-028c-4455-831d-1a5f32e33a49&query=Strawberry+Smoothie'>png image from freepik.com</a>"
+    }
+    else if (this.fruit == "Blueberry"){
+        smoothieCup.setAttribute("src", "/images/blueberry.png");
+        document.getElementById("imageAttr").innerHTML="<a href='https://www.freepik.com/free-vector/illustration-fruit-smoothie-drink-watercolor-style_2782931.htm#fromView=search&page=1&position=0&uuid=410ec575-b181-44fc-82cf-cffd9eff117b&query=blueberry+smoothie+clipart'>png image from freepik.com</a>"
+    }
+    else if (this.fruit == "Mango"){
+        smoothieCup.setAttribute("src", "/images/mango.png");
+        document.getElementById("imageAttr").innerHTML="<a href='https://www.freepik.com/free-vector/hand-painted-watercolor-smoothies-blender-glass-illustration_13839324.htm#fromView=keyword&page=1&position=0&uuid=dae3cd3d-028c-4455-831d-1a5f32e33a49&query=Strawberry+Smoothie'>png image from freepik.com</a>"
+    };
+    document.getElementById("image").appendChild(smoothieCup);
+    
+    }
 }
 
 document.getElementById("smoothieForm").addEventListener('submit', e => {
@@ -77,4 +108,5 @@ document.getElementById("smoothieForm").addEventListener('submit', e => {
     const order = new smoothieOrder(name, size, main, fruit, sizePrice, mainPrice, fruitPrice, Total);
     document.getElementById("result").textContent = order.description();
     document.getElementById("bill").textContent = order.bill();
+    order.serveIt();
 });
